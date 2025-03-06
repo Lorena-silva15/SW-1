5f39f21fd3990bb40279c3280ddd03312b3b1c41
Lorena Da Silva Rodrigues <159820934+Lorena-silva15@users.noreply.github.com>
2025-03-06 09:12:03 -0300

    Add files via upload

---
+++ Aula04/exercicio03/ex03.php
@@ -0,0 +1,19 @@
+<?php
+  
+$login=('login');
+$senha=('senha');
+
+if $login = "etec" && $senha= "informatica"{
+   echo "Logado com Sucesso";}
+
+else{
+   echo"Falha ao logar"
+}    
+
+  
+
+ 
+   
+
+
+?>
